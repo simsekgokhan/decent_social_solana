4d2099a992e9ba65fc2ab273217ac4f4f4edf45e
Gokhan Simsek <gokhansimsekfb@gmail.com>
2023-10-12 17:53:25 +0300

    First working program (Solana smart contract)

---
+++ program/src/lib.rs
@@ -0,0 +1,55 @@
+use borsh::{BorshDeserialize, BorshSerialize};
+use solana_program::{
+    account_info::{next_account_info, AccountInfo},
+    entrypoint,
+    entrypoint::ProgramResult,
+    msg,
+    pubkey::Pubkey,
+};
+
+entrypoint!(process_instruction);
+
+// This is the state managed by this program. This type must match the 
+// `UserProfile` type defined by the client/front_end
+#[derive(BorshSerialize, BorshDeserialize, Debug)]
+pub struct UserProfile {
+    pub blocked_account: bool,
+    pub xx: u32
+}
+
+#[derive(Copy, Clone)]
+enum ACTION {
+    CreateNewProfile = 1, 
+}
+
+pub fn process_instruction(
+    _program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8],
+) -> ProgramResult {
+    let accounts_iter = &mut accounts.iter();
+    let pda = next_account_info(accounts_iter)?;
+    msg!("--- instruction_data: {:?}", instruction_data);
+    msg!("--- pda: {}", pda.key);
+    msg!("--- pda.UserProfile: {:?}", UserProfile::try_from_slice(&pda.data.borrow())?);
+    // msg!("--- pda.data: {:?}", pda.data.borrow());
+    msg!("--- accounts.len {}", accounts.len());
+
+    // ACTION SELECTOR
+    // todo: move to instructions.rs for production uses
+    // todo: better use match but it will be two indentations and
+    //       it is more readable with if due to cast to u8
+    let fb = instruction_data[0]; // first byte
+    if fb == ACTION::CreateNewProfile as u8 {
+        msg!("--- instruction CreateNewProfile");
+        let mut program_data = UserProfile::try_from_slice(&pda.data.borrow())?;
+        program_data.blocked_account = false;
+        program_data.xx = 99;
+        program_data.serialize(&mut &mut pda.data.borrow_mut()[..])?;
+        msg!("--- CreateNewProfile Success");
+    }    else {
+        todo!() 
+    }
+
+    Ok(())
+}
